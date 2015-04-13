a3f13d2730a474e3228cebcae4316188107a6cb1
Dongchang He <dongch@chunkeey.cs.washington.edu>
2015-04-13 16:52:13 -0700

    initial upload

---
+++ cse446/src/cse446/ID3Tree.java
@@ -0,0 +1,13 @@
+package cse446;
+
+public class ID3Tree {
+	ID3TreeNode overallNode;
+	
+	public ID3Tree() {
+		
+	}
+	
+	public growTree() {
+	
+	}
+}
---
+++ cse446/src/cse446/ID3TreeNode.java
@@ -0,0 +1,9 @@
+package cse446;
+
+import java.util.ArrayList;
+
+public class ID3TreeNode {
+	public ArrayList<Integer> list;
+	
+
+}
